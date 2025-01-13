e4e59473f26c70b9a27c235dbd181d37f37c9790
jasmeendesai <130737953+jasmeendesai@users.noreply.github.com>
2025-01-13 23:17:14 +0530

    Update next.config.mjs

--- ev-dashboard/next.config.mjs
+++ ev-dashboard/next.config.mjs
@@ -1,11 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',  // Add this line
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+export default nextConfig;
